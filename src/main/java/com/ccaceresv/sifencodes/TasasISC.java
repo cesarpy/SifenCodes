ceb8d27f3df524770ade4244d2b0f53c363ec135
Cesar Caceres <cesarcaceres1994@gmail.com>
2018-06-25 18:10:54 -0400

    agregamos porcentaje

--- src/main/java/com/ccaceresv/sifencodes/TasasISC.java
+++ src/main/java/com/ccaceresv/sifencodes/TasasISC.java
@@ -4,28 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum TasasISC {
-    UNO(1,"01","1%"),
-    CINCO(2,"02","5%"),
-    NUEVE(3,"03","9%"),
-    DIEZ(4,"04","10%"),
-    ONCE(5,"05","11%"),
-    TRECE(6,"06","13%"),
-    DIECISEIS(7,"07","16%"),
-    DIECIOCHO(8,"08","18%"),
-    VEINTE(9,"09","20%"),
-    VEINTICUATRO(10,"10","24%"),
-    TREINTA_CUATRO(11,"11","34%"),
-    TREINTA_OCHO(12,"12","38%");
+    UNO(1,"01","1%",1),
+    CINCO(2,"02","5%",5),
+    NUEVE(3,"03","9%",9),
+    DIEZ(4,"04","10%",10),
+    ONCE(5,"05","11%",11),
+    TRECE(6,"06","13%",13),
+    DIECISEIS(7,"07","16%",16),
+    DIECIOCHO(8,"08","18%",18),
+    VEINTE(9,"09","20%",20),
+    VEINTICUATRO(10,"10","24%",24),
+    TREINTA_CUATRO(11,"11","34%",34),
+    TREINTA_OCHO(12,"12","38%",38);
 
     private Integer codigo;
     private String codigoStr;
     private String porcentaje;
+    private Integer porcentajeCodigo;
 
 
-    private TasasISC(Integer codigo, String codigoStr, String porcentaje) {
+    private TasasISC(Integer codigo, String codigoStr, String porcentaje,Integer porcentajeCodigo) {
         this.codigo = codigo;
         this.codigoStr = codigoStr;
         this.porcentaje = porcentaje;
+        this.porcentajeCodigo = porcentajeCodigo;
     }
 
     public Integer getCodigo() {
@@ -41,11 +43,13 @@ public enum TasasISC {
     }
 
     private static final Map<Integer, TasasISC> codeMap = new HashMap<>();
+    private static final Map<Integer, TasasISC> porcentajeMap = new HashMap<>();
 
     //vamos a cargar para poder buscar por codigo
     static {
         for(TasasISC ti : TasasISC.values()){
             codeMap.put(ti.getCodigo(), ti);
+            porcentajeMap.put(ti.porcentajeCodigo,ti);
         }
     }
 
@@ -54,6 +58,14 @@ public enum TasasISC {
             return null;
         }
         return codeMap.get(codigo);
+
+    }
+
+    public static TasasISC getByPorcentaje(Integer porcentaje){
+        if (porcentaje == null){
+            return null;
+        }
+        return porcentajeMap.get(porcentaje);
 
     }
 
